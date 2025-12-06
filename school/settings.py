e0cd76911bdfcab3eea6d4baa8921c5701c1b6f9
KlausPeace95 <klauspeace95@gmail.com>
2025-12-06 12:41:14 +0100

    dj-database-url add

--- school/settings.py
+++ school/settings.py
@@ -89,15 +89,19 @@ WSGI_APPLICATION = "school.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "scms",
+#         "USER": "postgres",
+#         "PASSWORD": "#Klaus#203#Klaus#",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "scms",
-        "USER": "postgres",
-        "PASSWORD": "#Klaus#203#Klaus#",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
